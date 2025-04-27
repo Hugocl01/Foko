5c48b6d1f9c1d90d7bf0a537b81cb9351c7b590f
Hugocl01 <hugocayon@gmail.com>
2025-04-27 21:12:54 +0200

    creacion de seeders u actualizacion de seeder de usuarios

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,14 +14,33 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // Usuario de pruebas
-        $u = new User();
-        $u->name = "Hugo";
-        $u->email = "hugocayon@gmail.com";
-        $u->password = "1234";
-        $u->plan_id = null;
-        $u->profile_picture_url = null;
-        $u->description = "Hola soy Hugo el crear de esta web espero que os gusta";
-        $u->save();
+        $users = [
+            [
+                'name' => 'Hugo',
+                'email' => 'hugocayon@gmail.com',
+                'password' => '1234',
+                'role' => 'user',
+                'description' => 'Hola soy Hugo, el creador de esta web, espero que os guste.',
+            ],
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => 'admin1234',
+                'role' => 'admin',
+                'description' => 'Administrador de la plataforma.',
+            ],
+        ];
+
+        foreach ($users as $userData) {
+            User::create([
+                'name' => $userData['name'],
+                'email' => $userData['email'],
+                'password' => Hash::make($userData['password']),
+                'role' => $userData['role'],
+                'plan_id' => null,
+                'profile_picture_url' => null,
+                'description' => $userData['description'],
+            ]);
+        }
     }
 }
